2400e0e87025b9c1217159cb7d83187a36d4d323
nurhan004 <sharshenbekov777@gmail.com>
2024-10-09 18:28:01 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,193 @@
+
+
+// const num1 = prompt("сан жаз:");
+// const num2 = prompt("сан жаз:");
+
+// if
+
+//  else if (num1 > num2) {
+//     alert( num1);
+// }  else if  (num1 < num2) {
+//     alert( num2); 
+// } else  (num1==num2) {
+//     alert("Экоо тен", num1, num2);
+
+// } 
+
+
+
+// const num = prompt("Сан жаз")
+
+// if (num % 2 === 0) {
+//     console.log("жуп сан");
+    
+// }
+// else   {
+
+//     console.log("так сан");
+    
+
+// }
+
+
+
+
+// const ball = prompt("Балынды жаз")
+
+// if (ball>= 100) {
+//     console.log("Степендия аласын!");
+    
+// } else if (ball>=60) {
+//     console.log("Кайра окуу");
+    
+// }
+// else {
+//     console.log("Сен жон эле койосунго");
+    
+// }
+
+
+
+
+
+// const grade = parseInt(prompt("Кансыл мезгил?"))
+
+// switch (grade) {
+//     case 1:
+//     console.log("Кыш айы");
+//     break;
+//     case 2:
+//     console.log("Кыш айы");
+//     break;
+//     case 3:
+//     console.log("Жаз айы");
+//     break;
+//     case 4:
+//     console.log("Жаз айы");
+//     break;
+//     case 5:
+//     console.log("Жаз айы");
+//     break;
+//     case 6:
+//     console.log("Жай айы");
+//     break;
+//     case 7:
+//     console.log("Жай айы");
+//     break;
+//     case 8:
+//     console.log("Жай айы");
+//     break;
+//     case 9:
+//     console.log("Куз айы");
+//     break;
+//     case 10:
+//     console.log("Куз айы");
+//     break;
+//     case 11:
+//     console.log("Куз айы");
+//     break;
+//     case 12:
+//     console.log("Кыш айы");
+//     break;
+//     default:
+//     console.log("Башка мезгил жок");
+//     break; 
+// }
+
+
+
+
+
+
+
+
+// const moon = parseInt(prompt("Канчанчы ай"))
+
+// switch (moon) {
+//     case 1:
+//     console.log("Январь");
+//     break;
+//     case 2:
+//     console.log("Февраль");
+//     break;
+//     case 3:
+//     console.log("Март");
+//     break;
+//     case 4:
+//     console.log("Апрель");
+//     break;
+//     case 5:
+//     console.log("Май");
+//     break;
+//     case 6:
+//     console.log("Июнь");
+//     break;
+//     case 7:
+//     console.log("Июль");
+//     break;
+//     case 8:
+//     console.log("Августь");
+//     break;
+//     case 9:
+//     console.log("Сентябрь");
+//     break;
+//     case 10:
+//     console.log("Октябрь");
+//     break;
+//     case 11:
+//     console.log("Ноябрь");
+//     break;
+//     case 12:
+//     console.log("Декабрь");
+//     break;
+//     default:
+//       console.log("Башка ай жок!");
+      
+
+
+    
+// }
+
+
+
+// const num1 = parseFloat(prompt("Введите первое число:"));
+// const operator = prompt("Введите оператор (*, /, -, +,>,<):");
+// const num2 = parseFloat(prompt("Введите второе число:"));
+// let result;
+// switch (operator) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       result = num1 / num2;
+//       break;
+//     case ">":
+//       result = num1 > num2;
+//       break;
+//     case "<":
+//       result = num1 < num2;
+//       break;
+//       default:
+//       alert("Неправильный оператор!");
+//       result = null;
+//   }
+  
+//   // Если результат корректный, выводим его в alert
+//   if (result !== null) {
+//     alert("Результат: " + result);
+//   }
+
+
+
+
+
+
+
+
+
